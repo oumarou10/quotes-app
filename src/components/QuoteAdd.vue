50ed90fe6d2edf2bd89838ce70fbc7eae6d3a35e
oumarucho <sissoko.oumarou1991@mail.com>
2018-02-09 20:35:01 +0100

    form component added

---
+++ src/components/QuoteAdd.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="row">
+        <form>
+            <div class="col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-3 col-lg-3 form-group">
+                <label>Quote</label>
+                <text-area class="form-control" v-model="quote"></text-area>
+            </div>
+            <div class="col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-3 col-lg-3 form-group">
+                <button class="btn btn-primary" @click="createNew">Add Quote</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "quote-add",
+        data: function () {
+            return {
+                quote: ''
+            }
+        },
+        methods: {
+            createNew(){
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
