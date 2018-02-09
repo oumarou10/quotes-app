e63266fcd7ad1ad75524dce6a758e21938ed0714
oumarucho <sissoko.oumarou1991@mail.com>
2018-02-09 17:48:26 +0100

    first quote created

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
     <div class="container">
-    
+        <app-quote :quotes="quotes"></app-quote>
     </div>
 </template>
 
 <script>
+    import Quote from './components/Quote.vue'
+
     export default {
-        
+        components: {
+          appQuote: Quote
+        },
+        data: function () {
+            return {
+                maxQuotes: 10,
+                quotes: [
+                    'Just a simple quote'
+                ]
+            }
+        }
     }
 </script>
 
---
+++ src/components/Quote.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="row">
+        <app-quote-show v-for="quote in quotes">{{ quote }}</app-quote-show>
+    </div>
+</template>
+
+<script>
+    import QuoteShow from './QuoteShow.vue'
+
+    export default {
+        name: "quote",
+        props: ['quotes'],
+        components: {
+            appQuoteShow: QuoteShow
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
